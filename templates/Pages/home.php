0aa3ed8960ae72f0454c228cecba52ff6a1a7afc
Hannah <lilypickle@hotmail.fr>
2021-04-09 23:54:04 +0200

    Dashboard -- get and use variables, layout + style

--- templates/Pages/home.php
+++ templates/Pages/home.php
@@ -1,6 +1,9 @@
+<?php
 
-<div class="jumbotron jumbotron-fluid p-4 ">
-  <div class="row px-4">
+?>
+
+<div class="jumbotron jumbotron-fluid p-3 mb-2">
+  <div class="row px-2 mx-1">
     <h1>Dashboard</h1>
   </div>
 </div>
@@ -17,27 +20,53 @@
     </button>
   </div>
 </div>
-<div class="container-fluid">
-  <div class="row">
-    <div class="col-3">
+<div class="container-fluid boxes">
+  <div class="row mt-5 mx-0" style="height:15%;">
 
-    </div>
-    <div class="col-6">
-      <div class="col-12">
-        <p>TOTAL</p>
+      <div class="col-3 h-100">
+        <div class="card text-center p-2">
+          <p class="Key-number purple-font font-weight-bold"><?=$query1[0]['count']?></p>
+          <p class="display-5">Adhérents à ASSO</p>
+        </div>
       </div>
-      <div class="col-12">
-        <canvas id="myChart" ></canvas>
+
+    <div class="col-6 h-100">
+      <div class="card p-0 overflow-hidden">
+        <div class="col-12 p-2">
+          <p class="Key-number purple-font font-weight-bold">
+            <?php
+            if($total>0){
+              echo "+".$total;
+            }else{
+              echo "-".$total;
+            }
+            ?>
+            €</p>
+            <p class="display-5">Solde opérations comptables</p>
+        </div>
+        <div class="col-12 p-0 dashboard chart-container">
+          <canvas id="myChart" data-amounts='<?=$amounts?>'></canvas>
+        </div>
       </div>
-      
     </div>
-    <div class="col-3">
+
+    <div class="col-3 h-100">
+        <div class="card text-center p-2">
+          <h5>Documents fréquents</h5>
+        </div>
     </div>
+
   </div>
-  <div class="row">
+  <div class="row mt-5 mx-0">
     <div class="col-6">
+      <div class="card text-center p-2">
+      <h5>Evenements à venir</h5>
+      </div>  
     </div>
     <div class="col-6">
+      <div class="card text-center p-2">
+        <h5>Derniers mouvements comptables</h5>
+      </div>
     </div>
   </div>
 </div>
