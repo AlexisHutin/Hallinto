dd172b0461029ed132781bd763858b93f1af1575
Hannah <lilypickle@hotmail.fr>
2021-05-07 13:58:21 +0200

    Navbar -- change logo to SVG

--- templates/element/nav.php
+++ templates/element/nav.php
@@ -1,9 +1,9 @@
 <?php
     $pages=[
         ['Dashboard', '','icon-home'],
-        ['Members', 'Users','icon-users'],
+        ['Members', 'Members','icon-users'],
         ['Événements', 'Events','icon-calendar'],
-        ['Statistiques', 'Statistiques','icon-bar-chart'],
+        ['Statistiques', 'Statistics','icon-bar-chart'],
         ['Comptabilité', 'AccountingEntries','icon-euro'],
         ['Documents & Aide', '','icon-file-text'],
         ['FAQ', '','icon-help-circle'],
@@ -13,11 +13,11 @@
 ?>
 
 
-<div class="col-2 left-nav p-3">
+<div class="col-2 left-nav  px-0">
 <div class="sticky-top">
     <nav class="navbar d-block p-0 text-center">
         <div class="my-4">
-            <?= $this->Html->image('hallinto-logo.png', ['alt' => 'Hallinto logo']); ?>
+            <?= $this->Html->image('hallinto_orange.svg', ['alt' => 'Hallinto logo']); ?>
         </div>
    
     <ul class="m-0 p-0 d-block text-left">
