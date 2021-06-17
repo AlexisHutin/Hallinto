dd1678cff5bc9bcfd43adff8f13277c5147c6c66
Hannah <lilypickle@hotmail.fr>
2021-06-17 13:37:01 +0200

    FAQ -- add menu Documents -- add menu

---
+++ templates/Informations/index_documents.php
@@ -0,0 +1,14 @@
+<div class="row h-100">
+  <?= $this->Element('nav') ?>
+  <div class="col-10 p-0">
+    <div id="accounting" class="pb-4 accountingEntries index content">
+        <div class="jumbotron jumbotron-fluid p-3 mb-2">
+            <div class="row px-2 mx-1">
+              <h1 class="th1-1">Documents & Aide</h1>
+            </div>
+        </div>
+    </div>
+    <div class="container-fluid">
+    </div>
+  </div>
+</div>
