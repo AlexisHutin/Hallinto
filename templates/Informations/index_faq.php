ab576bca45dad0412a9ca766dfedaccd28c1e025
Hannah <lilypickle@hotmail.fr>
2021-07-08 10:39:59 +0200

    FAQ -- adds layout

--- templates/Informations/index_faq.php
+++ templates/Informations/index_faq.php
@@ -1,7 +1,7 @@
 <div class="row h-100">
   <?= $this->Element('nav') ?>
   <div class="col-10 p-0">
-    <div id="accounting" class="pb-4 accountingEntries index content">
+    <div id="faq" class="pb-4 accountingEntries index content">
         <div class="jumbotron jumbotron-fluid p-3 mb-2">
             <div class="row px-2 mx-1">
               <h1 class="th1-1">FAQ</h1>
@@ -9,6 +9,68 @@
         </div>
     </div>
     <div class="container-fluid">
+      <div class="col-12">
+      <div class="container-fluid grey-border rounded-corners p-3 mb-4">
+          <div class="col-12 text-center pt-4 mt-2">
+            <form class="form-inline my-2 my-lg-0">
+              <input class="form-control search input-form col-11" type="search" placeholder="Je voudrais de l'aide par rapport à..." aria-label="Search">
+                <i class="icon-search orange-font col-1"></i>
+              </input>
+            </form>
+            <h2 class="orange-font th2-1 py-4 mt-2 ">Questions Fréquents</h2>
+            <p class="font-weight-bold">Quels sont les documents nécessaires pour créer une association? <span class="orange-font">|</span> Comment changer d'abonnement à Hallinto ?</p>
+          </div>
+        </div>
+      <div class="row py-3">
+        <div class="box col-6 py-3 d-inline-block">
+          <div  class="orange-border rounded-corners p-3 h-100">
+            <div class="text-center">
+              <i class="icon-users th1-1"></i>
+              <h3 class="th3-1 orange-font py-2">Création d'association</h3>
+              <p class="tx3 py-2">Comment créer une association avec Hallinto ? Retrouvez toutes nos explications sur la création d'association ici.</p>
+              <a href="#"> En savoir plus <i class="icon-log-out"></i></a>
+            </div>
+          </div>
+        </div>
+
+        <div class="box col-6 py-3 d-inline-block">
+          <div  class="orange-border rounded-corners p-3 h-100">
+            <div class="text-center">
+              <i class="icon-settings th1-1"></i>
+              <h3 class="th3-1 orange-font py-2">Gestion des adhérents</h3>
+              <p class="tx3 py-2">Comment ajouter une liste de membres ? Comment gérer les responsables de l’associations ? Nous répondons à toutes vos questions sur la gestion de membres.</p>
+              <a href="#"> En savoir plus <i class="icon-log-out"></i></a>
+            </div>
+          </div>
+        </div>
+
+        <div class="box col-6 py-3 d-inline-block">
+          <div class="orange-border rounded-corners p-3 h-100">
+            <div class="text-center">
+              <i class="icon-calendar th1-1"></i>
+              <h3 class="th3-1 orange-font py-2">Gestion d'évènements</h3>
+              <p class="tx3 py-2">Comment créer un évènement ? Comment rentrer les informations comptables sur l’évènement ? Comment gérer les différents états de l’évènement ?</p>
+              <a href="#"> En savoir plus <i class="icon-log-out"></i></a>
+            </div>
+          </div>
+        </div>
+
+        <div class="box col-6 py-3 d-inline-block">
+          <div  class="orange-border rounded-corners p-3 h-100">
+            <div class="text-center">
+              <i class="icon-users th1-1"></i>
+              <h3 class="th3-1 orange-font py-2">Utilisation de l'aide</h3>
+              <p class="tx3 py-2">Comment envoyer mes fichiers pour vérification ? Qui vérifie mes fichiers ? Combien de temps dois-je attendre pour récupérer mes fichiers corrigés ?</p>
+              <a href="#"> En savoir plus <i class="icon-log-out"></i></a>
+            </div>
+          </div>
+        </div>
+      </div>
+    
+    </div>
+
+
+
     </div>
   </div>
 </div>
